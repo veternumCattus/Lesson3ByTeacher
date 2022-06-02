744582af8d21b597a706f41d229bc603f33f7936
veternumCattus <sleepyc.d@gmail.com>
2022-06-03 00:38:33 +0300

    life cycle demo 1

--- app/src/main/java/ru/geekbrains/course1/lesson3byteacher/MainActivity.java
+++ app/src/main/java/ru/geekbrains/course1/lesson3byteacher/MainActivity.java
@@ -12,16 +12,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String instanceState;
-        if (savedInstanceState == null){
+        if (savedInstanceState == null) {
             instanceState = "Первый запуск";
-        }
-        else {
+        } else {
             instanceState = "Повторный запуск";
         }
         Toast.makeText(getApplicationContext(), instanceState + " - on Create", Toast.LENGTH_SHORT).show();
     }
+
     @Override
-    protected void onStart(){
-        super.onStart();
+    protected void onStart() {
+       super.onStart();
+        Toast.makeText(getApplicationContext(), "onStart", Toast.LENGTH_SHORT).show();
+   }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle saveInstanceState){
+        super.onRestoreInstanceState(saveInstanceState);
+        Toast.makeText(getApplicationContext(), "Повторный запуск!! - onRestoreInstanceState()", Toast.LENGTH_SHORT).show();
     }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(), "onResume()", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(), "onPause()",
+                Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle saveInstanceState){
+        super.onSaveInstanceState(saveInstanceState);
+        Toast.makeText(getApplicationContext(), "onSaveInstanceState()", Toast.LENGTH_SHORT).show();
+    }
+
 }
